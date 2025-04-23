2732387a4d238615295416b3f4c89222c2cefc3e
Jason <jayvdb1@gmail.com>
2025-04-23 11:25:08 +0200

    swift package release

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 
 import PackageDescription
 
-let tag = "v0.4.2"
-let checksum = "95ea5307eb3a99203e39cfa21d962bfe3e879e62429e8c7cdf5292cae5dc35cc"
+let tag = "v0.5.0"
+let checksum = "ecad0826f15016a3ff873098e988a40cc282558200334d07367b63a5f81ed8c2"
 let url = "https://github.com/lightningdevkit/ldk-node/releases/download/\(tag)/LDKNodeFFI.xcframework.zip"
 
 let package = Package(
